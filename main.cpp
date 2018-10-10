e74a4ca649ecf706ce1f25cee58d4073f42a0bcf
Elliot Brown <elliotjpb@gmail.com>
2018-10-10 20:38:37 +0100

    images are on top of each other

--- main.cpp
+++ main.cpp
@@ -15,28 +15,23 @@ using namespace std;
 using namespace cv;
 //To get homography from images passed in. Matching points in the images.
 
-Mat Stitching(Mat image1,Mat image2){
-
+Mat Homography(Mat image1, Mat image2){
     Mat I_1 = image1;
     Mat I_2 = image2;
-
     cv::Ptr<Feature2D> f2d = xfeatures2d::SIFT::create();
 
     	// Step 1: Detect the keypoints:
     	std::vector<KeyPoint> keypoints_1, keypoints_2;
     	f2d->detect( I_1, keypoints_1 );
     	f2d->detect( I_2, keypoints_2 );
-
     	// Step 2: Calculate descriptors (feature vectors)
     	Mat descriptors_1, descriptors_2;
     	f2d->compute( I_1, keypoints_1, descriptors_1 );
     	f2d->compute( I_2, keypoints_2, descriptors_2 );
-
     	// Step 3: Matching descriptor vectors using BFMatcher :
     	BFMatcher matcher;
     	std::vector< DMatch > matches;
     	matcher.match( descriptors_1, descriptors_2, matches );
-
     	// Keep best matches only to have a nice drawing.
     	// We sort distance between descriptor matches
     	Mat index;
@@ -49,8 +44,6 @@ Mat Stitching(Mat image1,Mat image2){
 
     	for (int i = 0; i < 200; i++)
     		bestMatches.push_back(matches[index.at < int > (i, 0)]);
-
-
     	// 1st image is the destination image and the 2nd image is the src image
     	std::vector<Point2f> dst_pts;                   //1st
     	std::vector<Point2f> source_pts;                //2nd
@@ -62,82 +55,72 @@ Mat Stitching(Mat image1,Mat image2){
     		source_pts.push_back( keypoints_2[ it->trainIdx ].pt );
     	}
 
-    	Mat H_12 = findHomography( source_pts, dst_pts, CV_RANSAC );
+    	Mat H = findHomography( source_pts, dst_pts, CV_RANSAC );
     	//cout << H_12 << endl;
+      return H;
+}
 
-      return H_12;
+void readme(){
+    std::cout << " Usage: ./SURF_descriptor <img1> <img2>" << std::endl;
 }
 
 /** @function main */
 int main(int argc, char** argv){
-  //Mats to get the first frame of video and pass to Stitching function.
-  Mat I1, h_I1;
-  Mat I2, h_I2;
 
-  // Create a VideoCapture object and open the input file
-  VideoCapture cap1("workingVideo/left.mov");
-  VideoCapture cap2("workingVideo/right.mov");
-  cap1.set(CV_CAP_PROP_BUFFERSIZE, 10);
-  cap2.set(CV_CAP_PROP_BUFFERSIZE, 10);
-  //Check if camera opened successfully
-  if(!cap1.isOpened() || !cap2.isOpened()){
-    cout << "Error opening video stream or file" << endl;
-    return -1;
+  if (argc != 5){
+      readme(); return -1;
   }
-//passing first frame to Stitching function
-  if (cap1.read(I1)){
-     h_I1 = I1;
-   }
 
-   if (cap2.read(I2)){
-     h_I2 = I2;
-   }
-   Mat homography;
-//passing here.
-   homography = Stitching(h_I1,h_I2);
-  std::cout << homography << '\n';
+  Mat im_1 = imread(argv[1], IMREAD_COLOR);
+  Mat im_2 = imread(argv[2], IMREAD_COLOR);
+  Mat im_3 = imread(argv[3], IMREAD_COLOR);
+  Mat im_4 = imread(argv[4], IMREAD_COLOR);
 
-//creating VideoWriter object with defined values.
-VideoWriter video("video/output.avi",CV_FOURCC('M','J','P','G'),30, Size(5760,2160));
+  if (!im_1.data || !im_2.data || !im_3.data || !im_4.data){
+      std::cout << " --(!) Error reading images " << std::endl; return -1;
+  }
 
+   //variable to store homography
+   Mat H_12, H_23, H_34;
+   //Finding homography of all images
+   H_12 = Homography(im_1,im_2);
+   H_23 = Homography(im_2,im_3);
+   H_34 = Homography(im_3,im_4);
 
-//Looping through frames of both videos.
-    for (;;){
-    Mat cap1frame;
-    Mat cap2frame;
+   std::cout << H_34 << '\n';
 
-    cap1 >> cap1frame;
-    cap2 >> cap2frame;
-
-    // If the frame is empty, break immediately
-    if (cap1frame.empty() || cap2frame.empty())
-      break;
       Mat warpImage2;
+      Mat warpImage3;
+      Mat warpImage4;
       //warpPerspective(cap2frame, warpImage2, homography, Size(cap1frame.cols*2, cap1frame.rows*2), INTER_CUBIC);
       //warping the second video cap2frame so it matches with the first one.
       //size is defined as the final video size
-      warpPerspective(cap2frame, warpImage2, homography, Size(cap1frame.cols*2, cap1frame.rows*2), INTER_CUBIC);
+      warpPerspective(im_2, warpImage2, H_12, Size(im_1.cols*2, im_1.rows*2), INTER_CUBIC);
+      warpPerspective(im_3, warpImage3, H_23, Size(im_1.cols*2, im_1.rows*2), INTER_CUBIC);
+      warpPerspective(im_4, warpImage4, H_34, Size(im_1.cols*2, im_1.rows*2), INTER_CUBIC);
       //std::cout << "cols " << (cap1frame.cols*2) << "rows " << (cap1frame.rows*2) << '\n';
       //final is the final canvas where both videos will be warped onto.
-      Mat final(Size(cap1frame.cols*2 + cap1frame.cols, cap1frame.rows*2),CV_8UC3);
+      Mat final(Size(im_1.cols*5 + im_1.cols, im_1.rows*5),CV_8UC3);
       //Mat final(Size(cap1frame.cols*2 + cap1frame.cols, cap1frame.rows*2),CV_8UC3);
       //Using roi getting the relivent areas of each video.
-      Mat roi1(final, Rect(0, 0,  cap1frame.cols, cap1frame.rows));
+      Mat roi1(final, Rect(0, 0, im_1.cols, im_1.rows));
       Mat roi2(final, Rect(0, 0, warpImage2.cols, warpImage2.rows));
+      Mat roi3(final, Rect(0, 0, warpImage3.cols, warpImage3.rows));
+      Mat roi4(final, Rect(0, 0, warpImage4.cols, warpImage4.rows));
       //warping images on to the canvases which are linked with the final canvas.
+      warpImage4.copyTo(roi4);
+      warpImage3.copyTo(roi3);
       warpImage2.copyTo(roi2);
-      cap1frame.copyTo(roi1);
-      int rows = final.rows;
-      int cols = final.cols;
-      //std::cout << "rows " << rows << "cols " << cols << '\n';
-      //writing to video.
-      video.write(final);
+      im_1.copyTo(roi1);
 
-      //imshow ("Result", final);
-    if(waitKey(30) >= 0) break;
-     //destroyWindow("Stitching");
-    // waitKey(0);
-  }
-  video.release();
-  return 0;
+      //int rows = final.rows;
+      //int cols = final.cols;
+      //std::cout << "rows " << rows << "cols " << cols << '\n';
+      imshow ("Result", final);
+      //imshow ("result", im_4);
+
+    //if(waitKey(30) >= 0) break;
+    //}
+    waitKey(0);
+    return 0;
 }
